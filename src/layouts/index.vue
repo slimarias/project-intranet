efa7b31d03e6075d7722ce380db24eb4a8a3c47a
Yeison Tapia <yftapial@ut.edu.co>
2019-08-14 11:09:02 -0500

    + update home layout

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="row">
     <div class="col-md-4">
-      <news/>
+      <news
+        :shadowVisible="true"
+        :titleVisible="true"/>
     </div>
     <div class="col-md-8">
       <div class="row">
