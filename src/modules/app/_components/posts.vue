fa0a9b7da918c3c901080383d8d91ca2557c1f7c
YeisonTapia <yftapial@ut.edu.co>
2020-06-25 12:18:09 -0500

    Add postList Component

--- src/modules/app/_components/posts.vue
+++ src/modules/app/_components/posts.vue
@@ -1,10 +1,17 @@
 <template>
-	<div class="row" style="border: 1px solid #f2f2f2">
+	<div class="row relative-position" style="border: 1px solid #f2f2f2">
 		<div class="col-12">
-			<q-list padding class="rounded-borders">
-				<q-item v-for="(post, key) in posts" :key="key" clickable v-ripple>
+			<q-list
+				padding
+				class="rounded-borders">
+				<q-item
+					v-for="(post, key) in posts"
+					:key="key"
+					@click="redirectTo(post)"
+					clickable
+					v-ripple>
 					<q-item-section top avatar>
-						<q-avatar rounded size="100px">
+						<q-avatar square size="100px">
 							<q-img
 								:src="post.mainImage.path"
 								style="width: 100px; height:100px; overflow: hidden"/>
@@ -14,37 +21,62 @@
 						<q-item-label class="text-h6 text-bold text-primary">
 							{{ post.title }}
 						</q-item-label>
-						<q-item-label class="text-justify">
+						<q-item-label class="text-justify line-clamp">
 							{{ post.summary }}
 						</q-item-label>
-						<q-item-label caption>
-							{{ post.createdAt }}
+						<q-item-label caption class="flex justify-end q-pt-sm">
+							{{ post.createdAt | date }}
 						</q-item-label>
 					</q-item-section>
 				</q-item>
 			</q-list>
 		</div>
 		
-		<div class="col-12 flex flex-center q-mb-md">
-			<q-btn color="primary" outline>
-				Mas
+		<div class="col-12 flex flex-center q-mb-md" v-if="loading">
+			<q-spinner
+				color="primary"
+				size="3em"
+				:thickness="2"
+			/>
+		</div>
+		
+		<div class="col-12 flex flex-center q-mb-md" v-if="!(this.page >= this.lastPage) && !loading">
+			<q-btn color="primary" outline @click="getMorePosts">
+				{{btnMoreLabel}}
 			</q-btn>
 		</div>
 	</div>
 </template>
 
 <script>
+  import moment from 'moment'
+  
   export default {
     name: "posts",
+	  props: {
+      categories: {
+        type: Array,
+	      default: () => []
+      },
+		  btnMoreLabel: {
+        type: String,
+        default: 'Mas'
+		  }
+	  },
 	  data() {
       return {
         loading: false,
         posts: [],
         page: 1,
-        take: 10,
+        take: 5,
         lastPage: 1,
       }
 	  },
+    filters:{
+      date: function (value){
+        return moment(value).format('d MMMM, YYYY')
+      }
+    },
 	  mounted() {
       this.$nextTick( () => {
         this.getPosts()
@@ -56,7 +88,7 @@
         const params = {
           params: {
             filter: {
-              //categories: this.idResource
+              categories: this.categories
             },
             include: 'category',
             page: this.page,
@@ -76,10 +108,28 @@
             this.loading = false
           })
       },
+	    getMorePosts(){
+        this.page = this.page + 1
+		    this.getPosts()
+	    },
+      redirectTo(post){
+        this.$router.push({
+          name: 'qblog.show',
+          params:{
+            category: post.category.slug,
+            slugPost: post.slug,
+          }
+        })
+      }
 	  }
   }
 </script>
 
 <style scoped>
-
+	.line-clamp {
+		display: -webkit-box;
+		-webkit-line-clamp: 3;
+		-webkit-box-orient: vertical;
+		overflow: hidden;
+	}
 </style>
