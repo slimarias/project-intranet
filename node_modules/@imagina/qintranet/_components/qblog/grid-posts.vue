f1285f0b60d2aca692d0883f48c76d1d5020b31e
YeisonTapia <yftapial@ut.edu.co>
2020-07-17 11:55:20 -0500

    Add qintranet module at @imagina

---
+++ node_modules/@imagina/qintranet/_components/qblog/grid-posts.vue
@@ -0,0 +1,64 @@
+<template>
+    <q-scroll-area style=" height: 700px;">
+      <div class="row">
+      <q-card
+        class="col-xs-12 col-md-4 no-shadow q-px-md q-mb-xl"
+        v-for="(item, index) in news"
+        :key="index">
+        <q-card-media>
+          <div
+            @click="handlerClick(item)"
+            class="product-img pointer"
+            :style="`background-image: url(${item.options.mainimage});`">
+          </div>
+        </q-card-media>
+        <q-card-title>
+          {{item.title}}
+          <span
+            slot="subtitle">
+            <b>{{$trd(item.createdAt)}}</b>
+          </span>
+        </q-card-title>
+      </q-card>
+      </div>
+    </q-scroll-area>
+  
+</template>
+
+<script>
+  export default {
+    props:{
+      news:{
+        type:Array,
+        default:()=>[]
+      }
+    },
+    data() {
+      return{
+      }
+    },
+    methods: {
+      handlerClick(item){
+        this.$router.push({
+          name: 'app.new',
+          params:{
+            newId: item.id
+          }
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import "~variables";
+  .product-img
+    background-position center
+    background-repeat no-repeat
+    background-size cover
+    height 300px
+    border 1px solid #dddddd52
+  
+  .pointer
+    cursor pointer
+</style>
---
+++ node_modules/@imagina/qintranet/_i18n/es/index.js
@@ -0,0 +1,7 @@
+import layout from './layout'
+import sidebar from './sidebar'
+
+export default {
+  layout,
+  sidebar
+}
