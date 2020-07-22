ef9686431e25c77c2f5d34216945a235d97bc9c1
Slim Arias Sanchez <slimarias1987@gmail.com>
2020-07-22 09:17:46 -0500

    Changes in news components

--- src/config/app.js
+++ src/config/app.js
@@ -22,6 +22,7 @@ export default {
     'qslider',
     'qbanner',
     'qsite',
+    'qchat',
     'app',
     'qintranet'
   ],
--- src/config/sidebar.js
+++ src/config/sidebar.js
@@ -1,9 +1,9 @@
 import coreSidebar from '@imagina/qsite/_config/master/sidebar'//Core sidebar [Do not remove]
 import pages from 'src/config/pages' // Get Pages from config
-
 //Add items tu sidebar
+console.log(pages)
 let responseSidebar = [
-  ...coreSidebar,
+  //...coreSidebar,
   //Add here more sidebar elements. #Example
   /*{
   title: "title",
@@ -17,6 +17,11 @@ let responseSidebar = [
         pages.groupPage.pageName, //Single Item in group items
       ]
     },*/
+  pages.qintranet.dashboard,
+  pages.qintranet.messenger,
+  pages.qintranet.news,
+  pages.qintranet.directory,
+  pages.qintranet.events,
 ]
 
 //Export sidebar
